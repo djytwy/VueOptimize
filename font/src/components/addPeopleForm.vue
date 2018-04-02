d4739cc6c42dcf53bdc67be564fef00e812510f5
unknown <676534074@qq.com>
2018-04-02 21:40:00 +0800

    最近更新的前端

---
+++ font/src/components/addPeopleForm.vue
@@ -0,0 +1,165 @@
+<template>
+	<div id="addPeopleForm">
+		<el-button type="primary" @click="show = !show" class="mybutton">添加人员</el-button>
+		<transition name="el-zoom-in-top">
+			<el-form :model="ruleForm2" v-show="show" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+			  	<el-form-item label="身份" prop="identity" class="form_this">
+			    	<el-select v-model="ruleForm2.identity" clearable placeholder="请选择身份" style="width:100%">
+					    <el-option
+					      v-for="item in options"
+					      :key="item.value"
+					      :label="item.label"
+					      :value="item.value">
+					    </el-option>
+					</el-select>
+			  	</el-form-item>
+			  	
+			  	<el-form-item label="姓名" prop="name" class="form_this">
+			    	<el-input v-model="ruleForm2.name" auto-complete="off" placeholder="请输入姓名"></el-input>
+			  	</el-form-item>
+
+			  	<el-form-item label="密码" prop="password" class="form_this">
+			    	<el-input type="password" v-model.number="ruleForm2.password" placeholder="请输入密码"></el-input>
+			  	</el-form-item>
+
+			  	<el-form-item label="手机号码" prop="phone" class="form_this">
+			    	<el-input v-model="ruleForm2.phone" auto-complete="off" placeholder="请输入手机号码"></el-input>
+			  	</el-form-item>
+
+			  	<el-form-item style="margin-left:-100px">
+			    	<el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+			    	<el-button @click="resetForm('ruleForm2')">重置</el-button>
+			  	</el-form-item>
+			</el-form>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+	import axios from 'axios'
+
+	export default {
+		name:'addPeopleForm',
+		data() {
+		    var checkIdentity = (rule, value, callback) => {
+		        if (!value) {
+		          	return callback(new Error('身份不能为空！'));
+		        }else{
+		        	callback ()
+		        }
+		    };
+		    var validateName = (rule, value, callback) => {
+		        if (!value) {
+		          	return callback(new Error('请输入姓名'));
+		        }else{
+		        	callback();
+		        }
+		    };
+		    var validatePassword = (rule, value, callback) => {
+		        if (!value) {
+		          	return callback(new Error('请输入密码'));
+		        } else {
+		          	callback();
+		        }
+		    };
+		    var validatePhone = (rule, value, callback) => {
+		        if (!value) {
+		          	return callback(new Error('请输入手机号码'));
+		        } else {
+		          setTimeout(() => {
+			          	const regPhone = RegExp(/0?(13|14|15|17|18|19)[0-9]{9}/)
+			          	if (regPhone.test(value) !== true){
+			          		return callback(new Error('请输入正确的手机号码！'))
+			          	}else{
+			          		return callback()
+			          	} 
+		          	},1000);
+		        }
+		    };
+		    return {
+		      	options: [{
+		          value: 1,
+		          label: '研发大神'
+		        }, {
+		          value: 2,
+		          label: '客服才子'
+		        }],
+		        ruleForm2: {
+		        	identity: '',
+		        	name: '',
+			        password: '', 
+			        phone:''
+		        },
+		        rules2: {
+		          password: [
+		            { validator: validatePassword, trigger: 'blur' }
+		          ],
+		          name: [
+		            { validator: validateName, trigger: 'blur' }
+		          ],
+		          identity: [
+		            { validator: checkIdentity, trigger: 'change' }
+		          ],
+		          phone: [
+		            { validator: validatePhone, trigger: 'blur' }
+		          ]
+		        },
+		        show:false,
+		    }
+		},
+		methods: {
+	      	submitForm(formName) {
+		        this.$refs[formName].validate((valid) => {
+		          if (valid) {
+		          	console.log("验证通过！")
+		          	const PeopleMessage = { 
+		          		"identity": this.ruleForm2.identity,"name": this.ruleForm2.name,
+		          		"password": this.ruleForm2.password,"phone": this.ruleForm2.phone
+		          	}
+
+		          	console.log(PeopleMessage)
+					axios.post('api/addPeople/',{
+						"PeopleMessage":PeopleMessage
+					})
+
+					.then(function (response) {
+						console.log("发送成功！");
+						resetForm(formName)
+					})
+
+					.catch(function (error) {
+						console.log(error);
+					})
+
+		            alert('submit!');
+		          } else {
+		            console.log('error submit!!');
+		            return false;
+		          }
+		        });
+		    },
+	      	resetForm(formName) {
+	        	this.$refs[formName].resetFields();
+	      	},
+	    }
+	}
+</script>
+
+<style scoped>
+	.form_this{
+		width: 30%;
+		margin-left: 32%; 
+	}
+	.mybutton{
+		margin-bottom: 30px;
+	}
+
+	.el-dropdown-link {
+	   	cursor: pointer;
+	   	color: #409EFF;
+	}
+	.el-icon-arrow-down {
+	   	font-size: 12px;
+	}
+</style>
---
+++ font/src/components/addPeopleTable.vue
@@ -0,0 +1,108 @@
+<template>
+	<div id="peopleTable" style="text-align:left;margin-left:28%">
+		<el-table
+		    :data="reverData"
+		    style="width: 64%"
+		    max-height="742">
+
+		    <el-table-column
+		  	  fixed="left"
+		      prop="name"
+		      label="姓名"
+		      width="352">
+		    </el-table-column>
+
+		    <el-table-column
+		      prop="work"
+		      label="干啥的？"
+		      width="302">
+		    </el-table-column>
+
+		    <el-table-column 
+		      label="操作"
+		      width="210">
+		      <template slot-scope="scope">
+		        <el-button
+		          @click.native.prevent="deleteRow(scope.$index, reverData)"
+		          type="text"
+		          size="small">
+		          移除
+		        </el-button>
+		      </template>      
+		    </el-table-column>
+
+		</el-table>
+	</div>
+</template>
+
+<script>
+
+	import axios from 'axios'
+
+	export default {
+		name:'addPeopleTable',
+		data(){
+			return {
+		  		tableData:[null],
+		  		responseData:[null],
+			} 
+		},
+
+		mounted:function(){
+			console.log("Vue初始化成功！")
+			this.$nextTick(function(){
+				var self = this
+
+				axios.get('/api/managePeople/')
+
+				.then(function(response){
+					self.responseData = response.data
+					console.log(self.responseData)
+				})
+
+				.catch(function(error){
+					console.log("Error!!!")
+				})
+
+			})
+		},
+
+		methods: {
+
+			deleteRow(index, rows) {
+	        	rows.splice(index, 1);
+	      	},
+
+		},
+		computed:{
+			reverData:function(){
+				var vm = this
+				if(vm.responseData[0] !== null ){
+					var tableData2 = []
+					for (let i in vm.responseData) {
+						if(vm.responseData[i]["i_groupid"] === 1){
+							vm.responseData[i]["work"] = "研发大神"
+						}else{
+							vm.responseData[i]["work"] = "客服才子"
+						}
+						vm.responseData[i]["name"] = vm.responseData[i]["s_username"]
+						if(vm.responseData[i]["s_show"] === 1){
+							tableData2.push(vm.responseData[i])
+						}
+					} 
+				}
+				return tableData2
+			}
+		},
+		// watch: {
+		// 	tableData:function() {
+		// 		console.log("获取请求的数据....")
+		// 		this.getdata()
+		// 	}
+		// }
+	}
+</script>
+
+<style scoped>
+	
+</style>
