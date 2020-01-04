bb169d60fd683692b2d15a1e2bebc8bcb8111185
djytwy <676534074@qq.com>
2020-01-05 00:23:03 +0800

    更改为部分引入

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/app'
+  ],
+  'plugins': [
+    [
+      'component',
+      {
+        'libraryName': 'element-ui',
+        'styleLibraryName': 'theme-chalk'
+      }
+    ]
   ]
 }
