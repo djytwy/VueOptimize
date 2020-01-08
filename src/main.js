f1d3d07a71644de359b4d19eb9d4678cb6e837f0
djytwy <676534074@qq.com>
2020-01-08 11:02:00 +0800

    删除main的引用，和一些无用代码

--- src/main.js
+++ src/main.js
@@ -3,18 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import {
-  Button,
-  // Select,
-  // Input,
-  // InputNumber,
-  // Radio,
-  // RadioGroup,
-} from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
+// import {
+//   Button,
+//   // Select,
+//   // Input,
+//   // InputNumber,
+//   // Radio,
+//   // RadioGroup,
+// } from 'element-ui'
 
 Vue.config.productionTip = false
-Vue.use(Button)
+// Vue.use(Button)
 // Vue.use(Select)
 // Vue.use(Input)
 // Vue.use(InputNumber)
